65feacb57651a560382d8c2e3a559ec40ca3134b
devBambu <byrr113@gmail.com>
2025-11-10 01:29:14 +0900

    [Initial Commit]

---
+++ ReptileDiary/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ReptileDiary
+//
+//  Created by 변예린 on 11/10/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
