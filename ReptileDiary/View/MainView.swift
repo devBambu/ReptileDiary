1cde3cf124785682a9e4fc90d968b304870c52c2
devBambu <byrr113@gmail.com>
2025-11-11 02:45:54 +0900

    [Design] Calendar 기초 UI 구현
    Calendar의 기초 토대 UI 구현 완료 - 현재 날짜와의 연동되도록 구현 필요

--- ReptileDiary/View/MainView.swift
+++ ReptileDiary/View/MainView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var selectedYear: String = "2025"
+    @State private var selectedMonth: String = "11"
+    
+    private let columns = Array(repeating: GridItem(.flexible()), count: 7)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Picker("Year", selection: $selectedYear) {
+                ForEach(Range<Int>(2000...2020)) {
+                    Text("\($0)")
+                }
+            } currentValueLabel: {
+                Text(selectedYear)
+            }
+            Picker("Month", selection: $selectedYear) {
+                ForEach(Range<Int>(1...12)) {
+                    Text("\($0)")
+                }
+            } currentValueLabel: {
+                Text(selectedMonth)
+            }
+        }
+        
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 5) {
+                ForEach(Range(0...200)) {
+                    Text("\($0)")
+                }
+                .frame(width: 40, height: 40)
+                .padding(1)
+                .background(Color.blue)
+                
+            }
+        }
+        .frame(width: 300, height: 300)
     }
 }
 
