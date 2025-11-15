74fb3831c90a940a1b6b6c541745102d1e20b8df
devBambu <byrr113@gmail.com>
2025-11-15 17:47:13 +0900

    [Design] Side Menu View 구현
    버튼 탭 시 사이드메뉴 나타나는 UI 구현

---
+++ ReptileDiary/SideMenuType.swift
@@ -0,0 +1,31 @@
+//
+//  SideMenuType.swift
+//  ReptileDiary
+//
+//  Created by 변예린 on 11/15/25.
+//
+
+import Foundation
+
+enum SideMenuRowType: Int, CaseIterable {
+    case calendar
+    case animal
+    
+    var title: String {
+        switch self {
+        case .calendar:
+            return "Calendar"
+        case .animal:
+            return "Animal"
+        }
+    }
+    
+    var iconName: String {
+        switch self {
+        case .calendar:
+            return "calendar"
+        case .animal:
+            return "lizard"
+        }
+    }
+}
--- ReptileDiary/View/MainView.swift
+++ ReptileDiary/View/MainView.swift
@@ -10,27 +10,43 @@ import SwiftUI
 struct MainView: View {
     let dateManager = DateManager.shared
     
+    @State var presentSideMenu: Bool = false
+    
     @State private var selectedYear: Int = 2025
     @State private var selectedMonth: Int = 11
     
     var body: some View {
-        HStack{
-            Image(systemName: "lizard.fill")
-            Text("마뱀일기")
-                .font(.title)
-                .fontWeight(.bold)
-            
+        ZStack {
+            VStack{
+                HStack {
+                    Spacer()
+                    Image(systemName: "lizard.fill")
+                    Text("마뱀일기")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button {
+                        presentSideMenu.toggle()
+                    } label: {
+                        Image(systemName: "text.justify")
+                    }
+
+                    Spacer()
+                }
+                
+                YearMonthPicker(year: $selectedYear, month: $selectedMonth, dateManager: dateManager)
+                CalendarGrid(year: selectedYear, month: selectedMonth, dateManager: dateManager)
+                //        Form {
+                //            List {
+                //                HStack{
+                //                    Image(systemName: "fork.knife.circle")
+                //                    Text("이름")
+                //                }
+                //            }
+                //        }
+            }
+            SideMenuView(isShowing: $presentSideMenu, content: AnyView(SideMenu()))
         }
-        YearMonthPicker(year: $selectedYear, month: $selectedMonth, dateManager: dateManager)
-        CalendarGrid(year: selectedYear, month: selectedMonth, dateManager: dateManager)
-//        Form {
-//            List {
-//                HStack{
-//                    Image(systemName: "fork.knife.circle")
-//                    Text("이름")
-//                }
-//            }
-//        }
     }
 }
 
@@ -99,3 +115,79 @@ struct CalendarGrid: View {
     }
 }
 
+struct SideMenuView: View {
+    @Binding var isShowing: Bool
+
+    var content: AnyView
+    var edgeTransition: AnyTransition = .move(edge: .trailing)
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            if isShowing {
+                Color.black
+                    .opacity(0.5)
+                    .ignoresSafeArea()
+                    .onTapGesture { isShowing.toggle() }
+                content
+                    .transition(edgeTransition)
+                    .background(Color.clear)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+        .ignoresSafeArea()
+        .animation(.easeInOut, value: isShowing)
+    }
+    
+}
+
+struct SideMenu: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            ZStack {
+                VStack(alignment: .trailing, spacing: 0) {
+                    ForEach(SideMenuRowType.allCases, id: \.self) { row in
+                        rowView(isSelected: true, imageName: row.iconName, title: row.title) {
+                            print(row.title)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.top, 100)
+                .frame(width: 270)
+                .background(Color.white)
+            }
+        }
+        .background(.clear)
+    }
+    
+    func rowView(isSelected: Bool, imageName: String, title: String, hideDivider: Bool = false, action: @escaping (() -> ())) -> some View {
+        Button {
+            action()
+        } label: {
+            VStack(alignment: .trailing) {
+                HStack(spacing: 20) {
+                    Rectangle()
+                        .fill(isSelected ? .purple : .white)
+                        .frame(width: 5)
+                    
+                    ZStack {
+                        Image(systemName: imageName)
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundStyle(isSelected ? .black : .gray)
+                            .frame(width: 26, height: 26)
+                    }
+                    .frame(width: 30, height: 30)
+                    Text(title)
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundStyle(isSelected ? .black : .gray)
+                    Spacer()
+                }
+            }
+        }
+        .frame(height: 50)
+        .background(LinearGradient(colors: [isSelected ? .purple.opacity(0.5) : .white, .white], startPoint: .leading, endPoint: .trailing))
+
+    }
+}
