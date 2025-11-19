8cd2ea5550b90e3ea21501d2a7a6bff83a389079
devBambu <byrr113@gmail.com>
2025-11-19 20:44:46 +0900

    [Feat] SwiftData 업데이트 기능 구현
    AddView 내부 update 기능 구현 - 플레이스홀더 수정 필요

--- ReptileDiary/View/AnimalView.swift
+++ ReptileDiary/View/AnimalView.swift
@@ -16,8 +16,10 @@ struct AnimalView: View {
         NavigationStack {
             List(animals) { animal in
                 HStack{
-                    Image(systemName: "lizard.fill")
-                    Text(animal.name)
+                    NavigationLink(destination: AddView(animal: animal)) {
+                        Image(systemName: "lizard.fill")
+                        Text(animal.name)
+                    }
                 }
                 .frame(height: 50)
 
@@ -51,6 +53,7 @@ struct AnimalView: View {
             }
             
         }
+
     }
 }
 
@@ -63,30 +66,40 @@ struct AnimalView: View {
 struct AddView: View {
 
     @Environment(\.modelContext) private var modelContext: ModelContext
+    @Query private var animals: [AnimalRecord]
    
     @State private var name = ""
     @State private var sex = ""
     @State private var weight = "0"
     
+    var animal: AnimalRecord?
+    
     var body: some View {
         Form {
-            Circle().frame(width: 80, height: 80)
-            Section("기본 정보") {
-                VStack {
-                    HStack {
-                        Text("이름: ")
-                        TextField("이름", text: $name)
-                    }
-                    HStack {
-                        Text("성별: ")
-                        TextField("성별", text: $sex)
-                    }
-                    HStack {
-                        Text("무게: ")
-                        TextField("무게", text: $weight)
-                    }
+            HStack {
+                Spacer()
+                Image(systemName: "lizard.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                Spacer()
+            }
+            Section("기본 정보") { // 플레이스홀더 수정 필요
+                    VStack {
+                        HStack {
+                            Text("이름: ")
+                            TextField(animal?.name ?? "이름", text: $name)
+                        }
+                        HStack {
+                            Text("성별: ")
+                            TextField(animal?.sex ?? "성별", text: $sex)
+                        }
+                        HStack {
+                            Text("무게: ")
+                            TextField("\(animal?.weight)", text: $weight) // 수정 필요
+                        }
                 }
-
             }
             
             HStack {
@@ -96,16 +109,32 @@ struct AddView: View {
                         print("유효하지 않은 무게")
                         return
                     }
-                    let newAnimal = AnimalRecord(name: name, sex: sex, weight: weight)
-                    modelContext.insert(newAnimal)
-                    do { try modelContext.save() }
-                    catch { print("저장 실패: \(error)")}
-                    print("확인완료")
+                    if let animal = animal {
+                        update(of: animal, name: name, sex: sex, weight: weight)
+                    } else {
+                        insert(name: name, sex: sex, weight: weight)
+                    }
                 }
                 Spacer()
             }
         }
-        .background(.white)
+        .scrollContentBackground(.hidden)
 
+    }
+    
+    func insert(name: String, sex: String, weight: Float) {
+        let newAnimal = AnimalRecord(name: name, sex: sex, weight: weight)
+        modelContext.insert(newAnimal)
+        do { try modelContext.save() }
+        catch { print("저장 실패: \(error)")}
+        print("확인완료")
+    }
+    
+    func update(of animal: AnimalRecord, name: String, sex: String, weight: Float) {
+        guard let idx = animals.firstIndex(of: animal) else { return }
+        animals[idx].name = name
+        animals[idx].sex = sex
+        animals[idx].weight = weight
+        try? modelContext.save()
     }
 }
